43616274e622a40cf2c935f7f1f1e0113ce402c2
Sebastian Hoffmann <43846468+sebifyy@users.noreply.github.com>
2020-05-28 21:09:05 +0200

    L06 initial upload

---
+++ L06 Server/js/L05_Client_Main.js
@@ -0,0 +1,61 @@
+"use strict";
+/*
+Aufgabe: Aufgabe 5
+Name: Sebastian Hoffmann
+Matrikel: 259316
+Datum: 23.05.2020
+Hiermit versichere ich, dass ich diesen Code zum Teil selbst, zum Teil inspiriert durch Kommilitonen auf der Pages Seite geschrieben habe...
+Durch diese Hilfe konnte ich einige Dinge verstehen.
+*/
+var HaushaltshilfeData;
+(function (HaushaltshilfeData) {
+    window.addEventListener("load", handleLoad);
+    async function handleLoad() {
+        let response = await fetch("data.json");
+        console.log(response);
+        let offer = await response.text();
+        let data = JSON.parse(offer);
+        HaushaltshilfeData.generateContent(data);
+        let form = document.querySelector("#form");
+        form.addEventListener("change", handleChange);
+        document.querySelector("#button")?.addEventListener("click", handleClick);
+    }
+    function handleClick() {
+        alert("Danke für deine Bestellung!");
+    }
+    function handleChange(_event) {
+        let order = document.querySelector("#order");
+        order.innerHTML = "";
+        let data = new FormData(document.forms[0]);
+        let total = 0;
+        for (let entry of data) {
+            if (entry[0] == "Produkte") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                let price = Number(item.getAttribute("price"));
+                let amount = Number(data.get(entry[1] + "Menge"));
+                let einheit = (item.getAttribute("einheit"));
+                total += price * amount;
+                order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
+            }
+            if (entry[0] == "Geld") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                let betrag = Number(data.get("Betrag"));
+                order.innerHTML += item.value + ": " + betrag + " €" + " <br>" + "<br>";
+                total += 5;
+            }
+            if (entry[0] == "Hausarbeiten") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                let price = Number(item.getAttribute("price"));
+                total += price;
+                order.innerHTML += item.value + ": " + price + " €" + "<br> <br>";
+            }
+            if (entry[0] == "Zahlung") {
+                let item = document.querySelector("[value='" + entry[1] + "']");
+                order.innerHTML += "<hr> Zahlungsmethode: " + item.value + "<br> <br>";
+            }
+        }
+        let supermarkt = data.get("Supermarkt");
+        order.innerHTML += "Supermarkt Vorgabe: " + supermarkt + "<br>" + "<hr>" + "<h3>Total(mit Gebühr): " + total + "€<h3><hr>";
+    }
+})(HaushaltshilfeData || (HaushaltshilfeData = {}));
+//# sourceMappingURL=L05_Client_Main.js.map
---
+++ L06 Server/js/L05_Client_Main.ts
@@ -0,0 +1,72 @@
+/*
+Aufgabe: Aufgabe 5
+Name: Sebastian Hoffmann
+Matrikel: 259316
+Datum: 23.05.2020
+Hiermit versichere ich, dass ich diesen Code zum Teil selbst, zum Teil inspiriert durch Kommilitonen auf der Pages Seite geschrieben habe... 
+Durch diese Hilfe konnte ich einige Dinge verstehen.
+*/
+namespace HaushaltshilfeData {
+
+    
+    window.addEventListener("load", handleLoad);
+
+    async function handleLoad(): Promise<void> {
+        let response: Response = await fetch("data.json");
+        console.log(response);
+        let offer: string = await response.text();
+        let data: Data = JSON.parse(offer);
+
+        generateContent(data);
+        let form: HTMLDivElement = <HTMLDivElement> document.querySelector("#form");
+        form.addEventListener("change", handleChange);
+        document.querySelector("#button")?.addEventListener("click", handleClick);
+    }
+
+    function handleClick(): void {
+        alert("Danke für deine Bestellung!");
+    }
+
+    function handleChange(_event: Event): void {
+        let order: HTMLDivElement = <HTMLDivElement> document.querySelector("#order");
+        order.innerHTML = "";
+
+        let data: FormData = new FormData(document.forms[0]);
+        let total: number = 0;
+
+        for (let entry of data) {
+            if (entry[0] == "Produkte") {
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let price: number = Number(item.getAttribute("price"));
+                let amount: number = Number (data.get(entry[1] + "Menge"));
+                let einheit: string = <string> (item.getAttribute("einheit"));
+                
+                total += price * amount;
+                order.innerHTML += item.value + " | " + amount + " " + einheit + ": " + price * amount + " €" + "<br> <br>";
+            }
+
+            if (entry[0] == "Geld") {
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let betrag: number = Number(data.get("Betrag"));
+                order.innerHTML += item.value + ": " +  betrag + " €" + " <br>" + "<br>";
+                total += 5;
+            }
+
+            if (entry[0] == "Hausarbeiten") {
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                let price: number = Number(item.getAttribute("price")); 
+                total += price;
+                order.innerHTML += item.value + ": " + price + " €" + "<br> <br>";
+            }
+
+            if (entry[0] == "Zahlung") {
+                let item: HTMLInputElement = <HTMLInputElement> document.querySelector("[value='" + entry[1] + "']");
+                order.innerHTML += "<hr> Zahlungsmethode: " + item.value + "<br> <br>";
+            }
+
+        }
+        let supermarkt: string = <string> data.get("Supermarkt");
+        order.innerHTML += "Supermarkt Vorgabe: " + supermarkt + "<br>" + "<hr>" + "<h3>Total(mit Gebühr): " + total + "€<h3><hr>" ;
+
+    }
+    }
