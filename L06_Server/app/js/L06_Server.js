f0f1268ad151a5a748753a3e4a1afa8794eacc3f
Sebastian Hoffmann <43846468+sebifyy@users.noreply.github.com>
2020-05-30 20:49:15 +0200

    port zu 8080 abgeändert

--- L06_Server/app/js/L06_Server.js
+++ L06_Server/app/js/L06_Server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
     var url_parts = url.parse(req.url, true);
     res.write(JSON.stringify(url_parts.query)); //write a response to the client
     res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+}).listen(8080); //the server object listens on port 80
 
 
 
