d9ee03cf3439fbcb8deb712e186e5731368f2cb7
Sebastian Hoffmann <43846468+sebifyy@users.noreply.github.com>
2020-05-30 20:00:30 +0200

    local funktionierts schonmal

--- L06 Server/app/js/L06_Server.js
+++ L06 Server/app/js/L06_Server.js
@@ -3,7 +3,7 @@ var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
     var url_parts = url.parse(req.url, true);
-    res.write(url_parts); //write a response to the client
+    res.write(JSON.stringify(url_parts.query)); //write a response to the client
     res.end(); //end the response
 }).listen(80); //the server object listens on port 80
 
