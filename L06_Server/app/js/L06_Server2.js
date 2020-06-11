63cc611a71caeb50fb3cc7b17f75c7db0999c29a
Sebastian Hoffmann <43846468+sebifyy@users.noreply.github.com>
2020-06-12 01:42:14 +0200

    L08 initial upload

--- L06_Server/app/js/L06_Server2.js
+++ L06_Server/app/js/L06_Server2.js
@@ -8,7 +8,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Http = __importStar(require("http"));
-//import { Http2Session } from "http2";
+const Url = __importStar(require("url"));
 var L06_Server;
 (function (L06_Server) {
     let server = Http.createServer();
@@ -16,10 +16,18 @@ var L06_Server;
     let port = process.env.PORT;
     if (port == undefined)
         port = 5001;
+    console.log("Server starting on:" + port);
     server.listen(port);
     server.addListener("request", handleRequest);
-    function handleRequest() {
+    function handleRequest(_request, _response) {
         console.log("Gehts?");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+        }
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write("My Response");
+        _response.end();
     }
 })(L06_Server = exports.L06_Server || (exports.L06_Server = {}));
 //# sourceMappingURL=L06_Server2.js.map
